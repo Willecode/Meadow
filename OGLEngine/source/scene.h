6eaad91b6ebaa68b1b9ef6f4fa4bf2d612fe5d28
Wille Kantelus <willecode@gmail.com>
2022-02-28 19:07:11 +0200

    scene draw passes camera matrices to shaders, scene keeps track of shaders used by objects

--- OGLEngine/source/scene.h
+++ OGLEngine/source/scene.h
@@ -1,14 +1,16 @@
 #pragma once
 #include "Object3D.h"
+#include "camera.h"
 #include <vector>
+#include <unordered_set>
 class Scene
 {
 public:
-	Scene();
+	Scene(Camera* camera);
 	void addObject(Object3D* obj);
 	void drawScene();
 	void updateLighting();
-
+	void updateShaders();
 private:
 	int objIdCounter;
 	std::vector<Object3D*> sceneObjects;
@@ -16,5 +18,8 @@ private:
 	int pointLightCount;
 	int dirLightCount;
 	std::unordered_map<Object3D*, LightSource*> sceneLights; // TODO: optimize this?
+
+	std::unordered_set<Shader*> sceneShaders;
+	Camera* sceneCamera;
 };
 
--- OGLEngine/source/shader_s.h
+++ OGLEngine/source/shader_s.h
@@ -14,7 +14,7 @@ public:
     unsigned int ID;
     // constructor generates the shader on the fly
     // ------------------------------------------------------------------------
-    Shader(const char* vertexPath, const char* fragmentPath)
+    Shader(const char* vertexPath, const char* fragmentPath): ID(-1)
     {
         // 1. retrieve the vertex/fragment source code from filePath
         std::string vertexCode;
