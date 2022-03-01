37cf40a437f4723614a7631359374d20c1a08c1c
Wille Kantelus <willecode@gmail.com>
2022-03-01 15:05:07 +0200

    Moved Camera class method definitions from h to cpp file

--- OGLEngine/source/camera.cpp
+++ OGLEngine/source/camera.cpp
@@ -1,1 +1,61 @@
 #include "camera.h"
+
+Camera::Camera(float aspect, float zNear, float zFar, glm::vec3 position, glm::vec3 up) :
+	aspect(aspect),
+	zNear(zNear),
+	zFar(zFar),
+	position(position),
+	yaw(-90.0f),
+	pitch(0.f),
+	worldUp(up),
+	speed(2.0f),
+	direction(0.f, 0.f, -1.0f),
+	lookSensitivity(10.0f),
+	fov(45.f)
+{
+}
+void Camera::processMouseMovement(float mouseOffsetX, float mouseOffsetY, float deltaTime) {
+	yaw += mouseOffsetX * lookSensitivity * deltaTime;
+	pitch += -mouseOffsetY * lookSensitivity * deltaTime;
+
+	direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	direction.y = sin(glm::radians(pitch));
+	direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+
+	direction = glm::normalize(direction);
+}
+glm::mat4 Camera::getViewMatrix() {
+
+	return glm::lookAt(position, position + direction, worldUp);
+}
+glm::mat4 Camera::getProjectionMatrix() {
+	return glm::perspective(glm::radians(fov), aspect, zNear, zFar);
+}
+void Camera::setFov(float fovIn) {
+	fov = fovIn;
+	if (fov < 1.0f)
+		fov = 1.0f;
+	if (fov > 45.0f)
+		fov = 45.0f;
+}
+float Camera::getFov() {
+	return fov;
+}
+void Camera::inputMoveUp(float deltaTime) {
+	position += worldUp * speed * deltaTime;
+}
+void Camera::inputMoveDown(float deltaTime) {
+	position -= worldUp * speed * deltaTime;
+}
+void Camera::inputMoveLeft(float deltaTime) {
+	position -= glm::normalize(glm::cross(direction, worldUp)) * speed * deltaTime;
+}
+void Camera::inputMoveRight(float deltaTime) {
+	position += glm::normalize(glm::cross(direction, worldUp)) * speed * deltaTime;
+}
+void Camera::inputMoveForward(float deltaTime) {
+	position += direction * speed * deltaTime;
+}
+void Camera::inputMoveBackward(float deltaTime) {
+	position -= direction * speed * deltaTime;
+}
--- OGLEngine/source/camera.h
+++ OGLEngine/source/camera.h
@@ -12,65 +12,18 @@ public:
 	float pitch;
 	float speed;
 	
-	Camera(float aspect, float zNear, float zFar, glm::vec3 position = glm::vec3(0.0f, 0.0f, 2.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f)) :
-		aspect(aspect),
-		zNear(zNear),
-		zFar(zFar),
-		position(position),
-		yaw(-90.0f),
-		pitch(0.f),
-		worldUp(up),
-		speed(2.0f),
-		direction(0.f, 0.f, -1.0f),
-		lookSensitivity(6.0f),
-		fov(45.f)
-	{
-	}
-	void processMouseMovement(float mouseOffsetX, float mouseOffsetY, float deltaTime) {
-		yaw += mouseOffsetX * lookSensitivity * deltaTime;
-		pitch += -mouseOffsetY * lookSensitivity * deltaTime;
-
-		direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-		direction.y = sin(glm::radians(pitch));
-		direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-
-		direction = glm::normalize(direction);
-	}
-	glm::mat4 getViewMatrix() {
-
-		return glm::lookAt(position, position + direction, worldUp);
-	}
-	glm::mat4 getProjectionMatrix() {
-		return glm::perspective(glm::radians(fov), aspect, zNear, zFar);
-	}
-	void setFov(float fovIn) {
-		fov = fovIn;
-		if (fov < 1.0f)
-			fov = 1.0f;
-		if (fov > 45.0f)
-			fov = 45.0f;
-	}
-	float getFov() {
-		return fov;
-	}
-	void inputMoveUp(float deltaTime) {
-		position += worldUp * speed * deltaTime;
-	}
-	void inputMoveDown(float deltaTime) {
-		position -= worldUp * speed * deltaTime;
-	}
-	void inputMoveLeft(float deltaTime) {
-		position -= glm::normalize(glm::cross(direction, worldUp)) * speed * deltaTime;
-	}
-	void inputMoveRight(float deltaTime) {
-		position += glm::normalize(glm::cross(direction, worldUp)) * speed * deltaTime;
-	}
-	void inputMoveForward(float deltaTime) {
-		position += direction * speed * deltaTime;
-	}
-	void inputMoveBackward(float deltaTime) {
-		position -= direction * speed * deltaTime;
-	}
+	Camera(float aspect, float zNear, float zFar, glm::vec3 position = glm::vec3(0.0f, 0.0f, 2.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f));
+	void processMouseMovement(float mouseOffsetX, float mouseOffsetY, float deltaTime);
+	glm::mat4 getViewMatrix();
+	glm::mat4 getProjectionMatrix();
+	void setFov(float fovIn);
+	float getFov();
+	void inputMoveUp(float deltaTime);
+	void inputMoveDown(float deltaTime);
+	void inputMoveLeft(float deltaTime);
+	void inputMoveRight(float deltaTime);
+	void inputMoveForward(float deltaTime);
+	void inputMoveBackward(float deltaTime);
 private:
 	float fov, aspect, zNear, zFar;
 };
