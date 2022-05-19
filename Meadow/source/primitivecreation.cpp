e8784467cbc532c8253bda37b049836ac2d4b55f
Wille Kantelus <willecode@gmail.com>
2022-05-19 11:31:07 +0300

    fix cube creation winding order

--- Meadow/source/primitivecreation.cpp
+++ Meadow/source/primitivecreation.cpp
@@ -9,43 +9,43 @@ std::unique_ptr<SubMesh> PrimitiveCreation::createCubeMesh()
     std::vector<Vertex> vertices;
     std::vector<unsigned int> indices;
                                        //pos                           //normal                       //texCoord
-    vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(0.0f, 0.0f)));
+    vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(0.0f, 0.0f))); // back
     vertices.push_back(Vertex(glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(1.0f, 0.0f)));
     vertices.push_back(Vertex(glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(1.0f, 1.0f)));
     vertices.push_back(Vertex(glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(0.0f, 1.0f)));
 
-    vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, 0.5f), glm::vec3( 0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 0.0f)));
+    vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, 0.5f), glm::vec3( 0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 0.0f))); // front
     vertices.push_back(Vertex(glm::vec3( 0.5f, -0.5f, 0.5f), glm::vec3( 0.0f, 0.0f, 1.0f), glm::vec2(1.0f, 0.0f)));
     vertices.push_back(Vertex(glm::vec3( 0.5f,  0.5f, 0.5f), glm::vec3( 0.0f, 0.0f, 1.0f), glm::vec2(1.0f, 1.0f)));
     vertices.push_back(Vertex(glm::vec3(-0.5f,  0.5f, 0.5f), glm::vec3( 0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 1.0f)));
 
-    vertices.push_back(Vertex(glm::vec3(-0.5f,  0.5f, 0.5f), glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 0.0f)));
+    vertices.push_back(Vertex(glm::vec3(-0.5f,  0.5f, 0.5f), glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 0.0f))); // left
     vertices.push_back(Vertex(glm::vec3(-0.5f,  0.5f,-0.5f), glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 1.0f)));
     vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f,-0.5f), glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 1.0f)));
     vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, 0.5f), glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f)));
 
-    vertices.push_back(Vertex(glm::vec3( 0.5f,  0.5f, 0.5f), glm::vec3( 1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 0.0f)));
+    vertices.push_back(Vertex(glm::vec3( 0.5f,  0.5f, 0.5f), glm::vec3( 1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 0.0f))); // right
     vertices.push_back(Vertex(glm::vec3( 0.5f,  0.5f,-0.5f), glm::vec3( 1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 1.0f)));
     vertices.push_back(Vertex(glm::vec3( 0.5f, -0.5f,-0.5f), glm::vec3( 1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 1.0f)));
     vertices.push_back(Vertex(glm::vec3( 0.5f, -0.5f, 0.5f), glm::vec3( 1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f)));
 
-    vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(0.0f, 1.0f)));
+    vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(0.0f, 1.0f))); // bottom
     vertices.push_back(Vertex(glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(1.0f, 1.0f)));
     vertices.push_back(Vertex(glm::vec3( 0.5f, -0.5f,  0.5f), glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(1.0f, 0.0f)));
     vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(0.0f, 0.0f)));
 
-    vertices.push_back(Vertex(glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(0.0f,  1.0f, 0.0f), glm::vec2(0.0f, 1.0f)));
+    vertices.push_back(Vertex(glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(0.0f,  1.0f, 0.0f), glm::vec2(0.0f, 1.0f))); // top
     vertices.push_back(Vertex(glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec3(0.0f,  1.0f, 0.0f), glm::vec2(1.0f, 1.0f)));
     vertices.push_back(Vertex(glm::vec3( 0.5f,  0.5f,  0.5f), glm::vec3(0.0f,  1.0f, 0.0f), glm::vec2(1.0f, 0.0f)));
     vertices.push_back(Vertex(glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec3(0.0f,  1.0f, 0.0f), glm::vec2(0.0f, 0.0f)));
     
-    indices = {
-        0, 1, 2, 0, 2, 3,
+    indices = { // consider the CCW winding order
+        0, 2, 1, 0, 3, 2,
         4, 5, 6, 4, 6, 7,
         8, 9, 10, 8, 10, 11,
-        12, 13, 14, 12, 14, 15,
+        12, 14, 13, 12, 15, 14,
         16, 17, 18, 16, 18, 19,
-        20, 21, 22, 20, 22, 23
+        20, 22, 21, 20, 23, 22
     };
     return std::make_unique<SubMesh>(vertices, indices, "Cube");
     
@@ -60,7 +60,7 @@ std::unique_ptr<SubMesh> PrimitiveCreation::createSphere(int sectorCount, int st
     float stackStep = pi / stackCount;
     float sectorAngle, stackAngle;
     float x, y, z;
-    //stackCount++; // Not sure why but this works
+
     for (int i = 0; i <= stackCount; i++) { 
         stackAngle = (-pi / 2) + i * stackStep;
         y = sinf(stackAngle);
