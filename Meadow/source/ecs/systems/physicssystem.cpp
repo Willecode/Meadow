9a0c5d09a3471000e04db3b6c1f5d1c3e857bb6e
Wille Kantelus <willecode@gmail.com>
2022-09-27 16:07:03 +0300

    physx intermediate commit

--- Meadow/source/ecs/systems/physicssystem.cpp
+++ Meadow/source/ecs/systems/physicssystem.cpp
@@ -1,1 +1,55 @@
 #include "PhysicsSystem.h"
+
+using namespace physx;
+void PhysicsSystem::init()
+{
+	m_PxFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, m_PxDefaultAllocator, m_PxDefaultErrorCallback);
+	m_PxPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *m_PxFoundation, PxTolerancesScale(), true);
+	if (!m_PxPhysics) {
+		throw("physics init failed");
+	}
+
+	// Create PhysX scene
+	PxSceneDesc sceneDesc(m_PxPhysics->getTolerancesScale());
+	sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+
+	// Multithreading
+	//m_PxDefaultCpuDispatcher = PxDefaultCpuDispatcherCreate(2);
+	//sceneDesc.cpuDispatcher = m_PxDefaultCpuDispatcher;
+	 
+	//sceneDesc.filterShader = PxDefaultSimulationFilterShader;
+	//m_PxScene = m_PxPhysics->createScene(sceneDesc);
+
+	// Call release() to delete objects
+
+	m_defaultMaterial = m_PxPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+
+	// Add a box
+	PxShape* shape = m_PxPhysics->createShape(PxBoxGeometry(1, 1, 1), *m_defaultMaterial);
+	PxTransform localTm(0,1,0);
+	PxRigidDynamic* body = m_PxPhysics->createRigidDynamic(localTm);
+	body->attachShape(*shape);
+	PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
+	m_PxScene->addActor(*body);
+	shape->release();
+
+	// Add a ground plane
+	PxRigidStatic* groundPlane = PxCreatePlane(*m_PxPhysics, PxPlane(0, 1, 0, 0), *m_defaultMaterial);
+	m_PxScene->addActor(*groundPlane);
+
+	m_timeAccumulate = 0.0f;
+	m_stepSize = 1.0f / 60.0f;
+
+}
+
+void PhysicsSystem::update(float deltaT, ECSCoordinator& ecs)
+{
+	m_timeAccumulate += deltaT;
+	if (m_timeAccumulate < m_stepSize)
+		return;
+
+	m_timeAccumulate -= m_stepSize;
+
+	m_PxScene->simulate(m_stepSize);
+	m_PxScene->fetchResults(true);
+}
--- Meadow/source/ecs/systems/physicssystem.h
+++ Meadow/source/ecs/systems/physicssystem.h
@@ -1,5 +1,24 @@
 #pragma once
-class PhysicsSystem
+#include "ecs/core/system.h"
+#include "ecs/core/ecscoordinator.h"
+#include <physx/PxPhysicsAPI.h>
+class PhysicsSystem : public System
 {
+public:
+	void init();
+	void update(float deltaT, ECSCoordinator& ecs);
+private:
+	physx::PxDefaultErrorCallback  m_PxDefaultErrorCallback;
+	physx::PxDefaultAllocator	   m_PxDefaultAllocator;
+	physx::PxDefaultCpuDispatcher* m_PxDefaultCpuDispatcher;
+	physx::PxFoundation*		   m_PxFoundation;
+	physx::PxPhysics*			   m_PxPhysics;
+	physx::PxPvd*				   m_PxPvd;
+	physx::PxScene*				   m_PxScene;
+
+	physx::PxMaterial* m_defaultMaterial;
+
+	float m_timeAccumulate;
+	float m_stepSize;
 };
 
